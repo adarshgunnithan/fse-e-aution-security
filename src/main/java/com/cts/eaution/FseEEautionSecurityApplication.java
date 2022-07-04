6ff3bd33ff679c582be806266c220860994e1526
aadi <aadi@localhost>
2022-07-05 00:34:03 +0900

    set up for security project

---
+++ src/main/java/com/cts/eaution/FseEEautionSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cts.eaution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FseEEautionSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FseEEautionSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cts/eaution/FseEEautionSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.eaution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FseEEautionSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
